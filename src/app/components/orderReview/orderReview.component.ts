1d72b18341d24aa59ce6d3193158b77f2585682a
elpaisa22 <sebastianecclesia@gmail.com>
2017-11-08 18:24:09 -0300

    Se muestra mensaje de error en checkout

--- src/app/components/orderReview/orderReview.component.ts
+++ src/app/components/orderReview/orderReview.component.ts
@@ -10,49 +10,66 @@ import {CheckoutRepository} from '../../repositories/checkout/checkout.repositor
 })
 export class OrderReviewComponent implements OnInit {
 
-		public subtotal : number;
-		public  items : CartItem[];
+	public subtotal : number;
+	public  items : CartItem[];
 
-    constructor(private cartService: CartService,
-			          private _checkoutRepository : CheckoutRepository,
-			          private router : Router) {
-    }
+	public hasErrors : boolean = false;
+	public errors : String;
 
-		//Se ejecuta al inicio
-		public ngOnInit() {
-			//Si aun no eligio el metodo de envio,redirige al metodo de envio
-			if (this.cartService.getDelivery() == null) {
-					this.router.navigate(['/delivery']);
-			//Si aun no eligio la direccion
-			} else if (this.cartService.getDelivery().address == null) {
-					this.router.navigate(['/address']);
-			//Si aun no eligio el metodo de pago, redirige al pago
-			} else if (this.cartService.getPayment() == null
-				         || this.cartService.getPayment() == null) {
-					this.router.navigate(['/payment']);
-			}
+  constructor(private cartService: CartService,
+					    private _checkoutRepository : CheckoutRepository,
+					    private router : Router) {
+  }
 
-			//Asigna la data desde el servicio
-			this.cartService.subtotalPrice.subscribe(data => this.subtotal = data);
-			this.cartService.items.subscribe(data => this.items = data);
+	//Se ejecuta al inicio
+	public ngOnInit() {
+		//Si aun no eligio el metodo de envio,redirige al metodo de envio
+		if (this.cartService.getDelivery() == null) {
+				this.router.navigate(['/delivery']);
+		//Si aun no eligio la direccion
+		} else if (this.cartService.getDelivery().address == null) {
+				this.router.navigate(['/address']);
+		//Si aun no eligio el metodo de pago, redirige al pago
+		} else if (this.cartService.getPayment() == null
+			         || this.cartService.getPayment() == null) {
+				this.router.navigate(['/payment']);
 		}
 
-		//Envia los datos al backend
-		public sendData() {
-			let result = this._checkoutRepository
-			                 .sendCheckoutData(this.cartService.getDelivery(),
-			                                   this.cartService.getPayment(),
-												                 this.cartService.getToken(),
-																				 this.items);
-			 // verifica el resultado
-       result.subscribe(
-               () => {
-                   this.router.navigate(['/home']);
-               },
-               err => {
-                   // Log errors if any
-                   console.log(err);
-               }
-			 );
-		}
+		//Asigna la data desde el servicio
+		this.cartService.subtotalPrice.subscribe(data => this.subtotal = data);
+		this.cartService.items.subscribe(data => this.items = data);
+	}
+
+	//Envia los datos al backend
+	public sendData() {
+		let result = this._checkoutRepository
+		                 .sendCheckoutData(this.cartService.getDelivery(),
+		                                   this.cartService.getPayment(),
+										   this.cartService.getToken(),
+										   this.items);
+
+		// verifica el resultado
+   	result.subscribe(
+           (res) => {
+           	   var response = res.json();
+           	   if (response.success) {
+           	   	this.router.navigate(['/home']);
+           	   }
+           },
+           err => {
+               // Log errors if any
+               //console.log(err);
+               this.showErrors(err)
+           }
+		);
+	}
+
+	private showErrors(msg : String) {
+		this.hasErrors = true;
+		this.errors = msg;
+	}
+
+	public closeErrors() {
+		this.hasErrors = false;
+	}
 }
