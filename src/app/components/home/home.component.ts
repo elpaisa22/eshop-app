c60537438d71b90f2105d6cf48ff32d01c7dd443
Ignacio Armendariz <armendariz.ignacio@gmail.com>
2018-11-07 16:42:22 -0300

    Se precargan los productos apenas se ingresa al sitio. Se cambia la info de contacto.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent implements OnInit {
 	}
 
   ngOnInit(){
+    this.productRepository.getProducts();
     this.offerRepository.getCurrentOffers()
                         .subscribe(data => {
                              this.currentOffers = data;
